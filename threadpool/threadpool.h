a03fdd98278dd68f63d73ccebb605029903d9480
cc <cc980101@tju.edu.cn>
2022-08-14 11:46:12 +0800

    commit_threadpool.h

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -1,152 +1,68 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-#include <list>
-#include <cstdio>
-#include <exception>
-#include <pthread.h>
-#include "../lock/locker.h"
-#include "../CGImysql/sql_connection_pool.h"
+//基于C++11 thread库线程池，跨平台，通过bind和function等特性使线程池执行任务更加通用化
 
-template <typename T>
-class threadpool
-{
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <thread>
+#include <functional>
+#include <assert.h>
+class ThreadPool {
 public:
-    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
-    threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
-    ~threadpool();
-    bool append(T *request, int state);
-    bool append_p(T *request);
+    explicit ThreadPool(size_t threadCount = 8): pool_(std::make_shared<Pool>()) {
+            assert(threadCount > 0);
+            for(size_t i = 0; i < threadCount; i++) {
+                std::thread([pool = pool_] {
+                    std::unique_lock<std::mutex> locker(pool->mtx);
+                    while(true) {
+                        if(!pool->tasks.empty()) {
+                            auto task = std::move(pool->tasks.front());
+                            pool->tasks.pop();
+                            locker.unlock();
+                            task();
+                            locker.lock();
+                        } 
+                        else if(pool->isClosed) break;
+                        else pool->cond.wait(locker);  //存在自动解锁加锁操作
+                    }
+                }).detach();
+            }
+    }
+
+    ThreadPool() = default;
+
+    ThreadPool(ThreadPool&&) = default;
+    
+    ~ThreadPool() {
+        if(static_cast<bool>(pool_)) {
+            {
+                std::lock_guard<std::mutex> locker(pool_->mtx);
+                pool_->isClosed = true;
+            }
+            pool_->cond.notify_all();
+        }
+    }
+
+    template<class F>
+    void AddTask(F&& task) {
+        {
+            std::lock_guard<std::mutex> locker(pool_->mtx);
+            pool_->tasks.emplace(std::forward<F>(task));
+        }
+        pool_->cond.notify_one();
+    }
 
 private:
-    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
-    static void *worker(void *arg);
-    void run();
-
-private:
-    int m_thread_number;        //线程池中的线程数
-    int m_max_requests;         //请求队列中允许的最大请求数
-    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
-    std::list<T *> m_workqueue; //请求队列
-    locker m_queuelocker;       //保护请求队列的互斥锁
-    sem m_queuestat;            //是否有任务需要处理
-    connection_pool *m_connPool;  //数据库
-    int m_actor_model;          //模型切换
+    struct Pool {
+        std::mutex mtx;
+        std::condition_variable cond;
+        bool isClosed;
+        std::queue<std::function<void()>> tasks;
+    };
+    std::shared_ptr<Pool> pool_;
 };
-template <typename T>
-threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
-{
-    if (thread_number <= 0 || max_requests <= 0)
-        throw std::exception();
-    m_threads = new pthread_t[m_thread_number];
-    if (!m_threads)
-        throw std::exception();
-    for (int i = 0; i < thread_number; ++i)
-    {
-        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
-        {
-            delete[] m_threads;
-            throw std::exception();
-        }
-        if (pthread_detach(m_threads[i]))
-        {
-            delete[] m_threads;
-            throw std::exception();
-        }
-    }
-}
-template <typename T>
-threadpool<T>::~threadpool()
-{
-    delete[] m_threads;
-}
-template <typename T>
-bool threadpool<T>::append(T *request, int state)
-{
-    m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
-    {
-        m_queuelocker.unlock();
-        return false;
-    }
-    request->m_state = state;
-    m_workqueue.push_back(request);
-    m_queuelocker.unlock();
-    m_queuestat.post();
-    return true;
-}
-template <typename T>
-bool threadpool<T>::append_p(T *request)
-{
-    m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
-    {
-        m_queuelocker.unlock();
-        return false;
-    }
-    m_workqueue.push_back(request);
-    m_queuelocker.unlock();
-    m_queuestat.post();
-    return true;
-}
-template <typename T>
-void *threadpool<T>::worker(void *arg)
-{
-    threadpool *pool = (threadpool *)arg;
-    pool->run();
-    return pool;
-}
-template <typename T>
-void threadpool<T>::run()
-{
-    while (true)
-    {
-        m_queuestat.wait();
-        m_queuelocker.lock();
-        if (m_workqueue.empty())
-        {
-            m_queuelocker.unlock();
-            continue;
-        }
-        T *request = m_workqueue.front();
-        m_workqueue.pop_front();
-        m_queuelocker.unlock();
-        if (!request)
-            continue;
-        if (1 == m_actor_model)
-        {
-            if (0 == request->m_state)
-            {
-                if (request->read_once())
-                {
-                    request->improv = 1;
-                    connectionRAII mysqlcon(&request->mysql, m_connPool);
-                    request->process();
-                }
-                else
-                {
-                    request->improv = 1;
-                    request->timer_flag = 1;
-                }
-            }
-            else
-            {
-                if (request->write())
-                {
-                    request->improv = 1;
-                }
-                else
-                {
-                    request->improv = 1;
-                    request->timer_flag = 1;
-                }
-            }
-        }
-        else
-        {
-            connectionRAII mysqlcon(&request->mysql, m_connPool);
-            request->process();
-        }
-    }
-}
-#endif
+
+
+#endif //THREADPOOL_H
